5df42965ec458f03e2c8c6cba0c5fd543d2b1347
James <josueyat318@gmail.com>
2024-11-04 17:13:09 +0800

    commit

---
+++ wallet.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from database import Database
+
+class INIT:
+    def create_wallet():
+        Database().insert()
+        
+
+class Wallet:
+    remaining = 0
+    
+    def buy_in_history(self):
+        return
+    
+    
+    def cash_out_history(self):
+        return
+    
+    
+    def get_remaining():
+        return
+    
+    
+    def get_assets():
+        return
+    
+    
+    def performance():
+        return
+    
+    
